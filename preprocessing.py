f5b1a22cdde1fce677f5f8d6751e34c2d742a153
Sanchit Ram Arvind <sanchitram@gmail.com>
2025-11-11 13:39:52 -0800

    preprocessing

---
+++ preprocessing.py
@@ -0,0 +1,21 @@
+import re
+
+import nltk
+from nltk.corpus import stopwords
+
+# Download stopwords if not already
+nltk.download("stopwords")
+
+
+# Preprocess function
+stop_words = set(stopwords.words("english"))
+
+
+def preprocess(text):
+    """Remove the useless words from the question"""
+    text = text.lower()
+    text = re.sub(r"[^\w\s]", "", text)
+    words = text.split()
+    words = [w for w in words if w not in stop_words]
+    return " ".join(words)
+    return " ".join(words)
